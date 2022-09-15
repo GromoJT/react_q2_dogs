a2bda1ef6b43bcf72f197336f25235e1631b09ed
GromoJT <85227567+GromoJT@users.noreply.github.com>
2022-09-15 02:01:19 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-import Dog from "./Dog";
+const Dog = React.lazy(()=>import('./Dog'));
 
 const App = () => (
   <div className="container">
-
+  <Suspense fallback={<div>Loading...</div>}>
   <Dog />
+  </Suspense>
+  
 
     
   </div>
